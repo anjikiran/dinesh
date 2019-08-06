8b7ccb0ba60b0355c5f0e5fc8b79c3ef3dd39507
anjikiran <anji.kiran@gmail.com>
2019-08-06 15:30:08 +0530

    Initial commit

---
+++ Maven Projects/testing/src/main/java/com/Flextrio/App.java
@@ -0,0 +1,14 @@
+package com.Flextrio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Maven Projects/testing/src/test/java/com/Flextrio/AllTests.java
@@ -0,0 +1,11 @@
+package com.Flextrio;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AppTest.class })
+public class AllTests {
+
+}
